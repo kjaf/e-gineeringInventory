a35e54e328cd79907ad2572e7b6ef3ca06b697b7
Kyle Fetterman <Kyle_Fetterman@wvlcorp.com>
2021-08-19 13:05:30 -0400

    adding query client provider

--- reactworkshopkylefetterman/src/App.tsx
+++ reactworkshopkylefetterman/src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
+import Nav from './Nav'
 import { BrowserRouter, Route } from 'react-router-dom'
 import About from './About'
 import AddFoodForm from './AddFoodForm'
 import Home from './Home'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 const App = () => {
+    const queryClient = new QueryClient();
+
     return (
         <>
-        <BrowserRouter>
-<Nav />
-    <Route path="/about"><About /></Route> 
-    <Route path="/" exact><Home /></Route> 
-    <Route path="/addFood"><AddFoodForm /></Route>
-    <Route path="/food/:foodId" ><AddFoodForm /></Route>
-</BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+            <Nav />
+        <Route path="/about"><About /></Route> 
+        <Route path="/" exact><Home /></Route> 
+        <Route path="/addFood"><AddFoodForm /></Route>
+        <Route path="/food/:foodId" ><AddFoodForm /></Route>
+    </BrowserRouter>
+    </QueryClientProvider>
 </>
     )
 }
